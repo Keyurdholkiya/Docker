210286b6a3a6d70e2c2d47ac49cffa4064217dc0
Keyurdholkiya <keyurdholkiya2005@gmail.com>
2026-06-04 12:57:49 +0530

    try on new language docker

---
+++ python/webapp/web.py
@@ -0,0 +1,13 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class MyHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(b"<h1>Hello Keyur! Docker is Working.</h1>")
+
+server = HTTPServer(("0.0.0.0", 8000), MyHandler)
+
+print("Server running on port 8000...")
+server.serve_forever()
